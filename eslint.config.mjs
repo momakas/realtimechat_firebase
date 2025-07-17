4538b1db5c5b4d0dbde61da0a03f7770f6ead4ca
momakas <kkgtycb@yahoo.co.jp>
2025-07-18 02:59:43 +0900

    疎通確認

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,34 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  // 追記
+  {
+    ignores: ['**/.next/**', '**/_next/**', '**/dist/**', '**/storybook-static/**', '**/lib/**'],
+  },
+  ...compat.extends('next/core-web-vitals', 'prettier'),
+  {
+    rules: {
+      'react/jsx-sort-props': [
+        'error',
+        {
+          callbacksLast: true,
+          shorthandLast: true,
+        },
+      ],
+      'import/order': 'error',
+    },
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
